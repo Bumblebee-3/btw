dac1278e3d729de184200dfa9cd87e9d38b3a9d6
Bumblebee-3 <bumblebee4ever05@gmail.com>
2025-12-26 19:41:37 +0530

    update env.sh

--- scripts/env.sh
+++ scripts/env.sh
@@ -1,4 +1,8 @@
+
 export GROQ_API_KEY=""
 export MISTRAL_API_KEY=""
 export GEMINI_API_KEY=""
+
+#comment out the line below after setting keys in this file
+source "$(cd "$(dirname "$0")" >/dev/null 2>&1 && pwd)/old.sh"
 
